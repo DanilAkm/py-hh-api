3290cc0823dac2bcb1fe92518deb185d52569ac7
boyceing <akmd.uk@gmail.com>
2025-03-16 19:12:47 +0000

    linter3

--- main.py
+++ main.py
@@ -19,35 +19,35 @@ app = HH.App(client_id, client_secret, 'boyceing/1 (boyceing@boyceing.ru)', 'hh.
 target_user = 'akmd.uk@gmail.com'
 userinfo = users.find_one({'email': target_user}, {'_id': False})
 
-#  User not found in database
+#    User not found in database
 if userinfo is None:
-  print(app.get_link_for_authcode())
-  emp = HH.Employee(app, code=input())
+    print(app.get_link_for_authcode())
+    emp = HH.Employee(app, code=input())
 
-  user_profile = emp.get_info()
-  user_profile['access_token'] = emp.access_token
-  user_profile['refresh_token'] = emp.refresh_token
-  user_profile['expires_at'] = emp.expires_at
+    user_profile = emp.get_info()
+    user_profile['access_token'] = emp.access_token
+    user_profile['refresh_token'] = emp.refresh_token
+    user_profile['expires_at'] = emp.expires_at
 
-  users.insert_one(user_profile)
+    users.insert_one(user_profile)
 
-#  Found. If token expired - renew 
+#    Found. If token expired - renew 
 else:
-  emp = core.Employee(app, employee_data=userinfo)
-  if userinfo['expires_at'] < time.time():
-    emp.renew_token()
-    query_filter = {'email' : target_user}
-    update_operation = { '$set' : {
-        'access_token': emp.access_token,
-        'refresh_token': emp.refresh_token,
-        'expires_at': emp.expires_at
-      }
-    }
-    result = users.update_one(query_filter, update_operation)
+    emp = HH.Employee(app, employee_data=userinfo)
+    if userinfo['expires_at'] < time.time():
+        emp.renew_token()
+        query_filter = {'email' : target_user}
+        update_operation = { '$set' : {
+                'access_token': emp.access_token,
+                'refresh_token': emp.refresh_token,
+                'expires_at': emp.expires_at
+            }
+        }
+        result = users.update_one(query_filter, update_operation)
 
 # https://api.hh.ru/openapi/redoc#tag/Poisk-vakansij-dlya-soiskatelya/operation/get-vacancies-similar-to-resume
 search_filter = {
-  'salary': 200000
+    'salary': 200000
 }
 
 resume_id = emp.get_resumes()['items'][0]['id']
