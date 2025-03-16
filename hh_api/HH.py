891d0f0da6c1089727534690aaa9605242b3b644
boyceing <akmd.uk@gmail.com>
2025-03-16 19:20:37 +0000

    linter docstrings

--- hh_api/HH.py
+++ hh_api/HH.py
@@ -2,16 +2,40 @@ import requests
 import json
 from time import time
 
+"""
+Module for interacting with the HH.ru API.
+
+This module provides classes to authenticate and retrieve information 
+about applications and employees using the HH.ru API.
+"""
+
 HH_API_BASE='https://api.hh.ru'
 
 class App:
+    """
+    Represents an application interacting with the HH.ru API.
+
+    Handles authentication and provides methods to retrieve application-related data.
+    """
+    
     def __init__(self, client_id: str,
                  client_secret: str,
                  client_info: str,
                  host: str,
                  locale='RU',
                  app_token=None):
+        """
+        Initializes the App instance.
 
+        Args:
+            client_id (str): The client ID for authentication.
+            client_secret (str): The client secret for authentication.
+            client_info (str): Information about the client application.
+            host (str): The host locale for API requests.
+            locale (str, optional): The locale setting. Defaults to 'RU'.
+            app_token (str, optional): An existing application token. Defaults to None.
+        """
+        
         self.client_id = client_id
         self.client_secret = client_secret
         self.client_info = client_info
@@ -30,6 +54,13 @@ class App:
         self.app_token = app_token
 
     def get_app_info(self):
+        """
+        Retrieves information about the authenticated application.
+
+        Returns:
+            dict: Application details from the API.
+        """
+        
         headers = {
             'Authorization': f'Bearer {self.app_token}',
             'HH-User-Agent': self.client_info
@@ -42,12 +73,35 @@ class App:
         return response.json()
 
     def get_link_for_authcode(self):
+        """
+        Generates a link for user authorization.
+
+        Returns:
+            str: The authorization URL.
+        """
+        
         return f'https://hh.ru/oauth/authorize?response_type=code&client_id={self.client_id}'
 
 class Employee:
+    """
+    Represents an authenticated employee on the HH.ru platform.
 
+    Handles authentication, token management, and provides methods to retrieve employee-related data.
+    """
+    
     def __init__(self, app: App, code=None, employee_data=None):
+        """
+        Initializes the Employee instance.
 
+        Args:
+            app (App): The application instance for authentication.
+            code (str, optional): The authorization code for obtaining a token.
+            employee_data (dict, optional): Existing employee authentication data.
+
+        Raises:
+            TypeError: If app is not an instance of App or if required authentication data is missing.
+        """
+        
         if not isinstance(app, App):
             raise TypeError("Expected an instance of App")
 
@@ -78,6 +132,10 @@ class Employee:
         self.appdata = app
 
     def renew_token(self):
+        """
+        Renews the employee's authentication token using the refresh token.
+        """
+        
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -94,6 +152,13 @@ class Employee:
         self.expires_at = time() + response['expires_in']
 
     def invalidate_token(self):
+        """
+        Invalidates the employee's authentication token.
+
+        Returns:
+            dict: API response confirming token invalidation.
+        """
+        
         headers = {
             'Authorization': f'Bearer {self.access_token}'
         }
@@ -101,6 +166,13 @@ class Employee:
         return response.json()
 
     def get_info(self):
+        """
+        Retrieves information about the authenticated employee.
+
+        Returns:
+            dict: Employee details from the API.
+        """
+        
         headers = {
             'HH-User-Agent': self.appdata.client_info,
             'Authorization': f'Bearer {self.access_token}'
@@ -113,6 +185,13 @@ class Employee:
         return response.json()
 
     def get_resumes(self):
+        """
+        Retrieves the authenticated employee's resumes.
+
+        Returns:
+            dict: List of resumes associated with the employee.
+        """
+        
         headers = {
             'HH-User-Agent': self.appdata.client_info,
             'Authorization': f'Bearer {self.access_token}'
@@ -125,6 +204,17 @@ class Employee:
         return response.json()
 
     def get_vacancies_for_resume(self, resume_id: str, params: dict):
+        """
+        Retrieves similar vacancies for a given resume.
+
+        Args:
+            resume_id (str): The ID of the resume.
+            params (dict): Additional parameters for filtering vacancies.
+
+        Returns:
+            dict: List of similar vacancies.
+        """
+        
         headers = {
             'HH-User-Agent': self.appdata.client_info,
             'Authorization': f'Bearer {self.access_token}'
