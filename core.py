4c7d3e1e8c75163856e9de9f11cf05c68ea65bbb
boyceing <akmd.uk@gmail.com>
2025-03-16 17:29:41 +0000

    init commit

---
+++ core.py
@@ -0,0 +1,136 @@
+import requests
+import json
+from time import time
+
+HH_API_BASE='https://api.hh.ru'
+
+class App:
+  def __init__(self, client_id: str, client_secret: str, client_info: str, host: str, locale='RU', app_token=None):
+
+    self.client_id = client_id
+    self.client_secret = client_secret
+    self.client_info = client_info
+    self.host = host
+    self.locale = locale
+
+    data = {
+      'client_id': client_id,
+      'client_secret': client_secret,
+      'grant_type': 'client_credentials'
+    }
+
+    if app_token is None:
+      response = requests.post(url=f'{HH_API_BASE}/token', data=data)
+      app_token = response.json()['access_token']
+    self.app_token = app_token
+
+  def get_app_info(self):
+    headers = {
+      'Authorization': f'Bearer {self.app_token}',
+      'HH-User-Agent': self.client_info
+    }
+    params = {
+      'locale': self.locale,
+      'host': self.host
+    }
+    response = requests.get(f'{HH_API_BASE}/me', params=params, headers=headers)
+    return response.json()
+  
+  def get_link_for_authcode(self):
+    return f'https://hh.ru/oauth/authorize?response_type=code&client_id={self.client_id}'
+  
+class Employee:
+
+  def __init__(self, app: App, code=None, employee_data=None):
+
+    if not isinstance(app, App):
+      raise TypeError("Expected an instance of App")
+
+    if employee_data is None:
+      if code is None:
+        raise TypeError("Missing auth code")
+      headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+
+      data = {
+        'code': code,
+        'client_id': app.client_id,
+        'client_secret': app.client_secret,
+        'grant_type': 'authorization_code'
+      }
+
+      response = requests.post(url=f'{HH_API_BASE}/token', headers=headers ,data=data).json()
+      print(response)
+      self.access_token = response['access_token']
+      self.refresh_token = response['refresh_token']
+      self.expires_at = time() + response['expires_in']
+    else:
+      self.access_token = employee_data['access_token']
+      self.refresh_token = employee_data['refresh_token']
+      self.expires_at = time() + employee_data['expires_at']
+
+    self.appdata = app
+
+
+
+  def renew_token(self):
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    data = {
+      'refresh_token': self.refresh_token,
+      'grant_type': 'refresh_token'
+    }
+
+    response = requests.post(url=f'{HH_API_BASE}/token', headers=headers, data=data).json()
+    print(response)
+    self.access_token = response['access_token']
+    self.refresh_token = response['refresh_token']
+    self.expires_at = time() + response['expires_in']
+  
+  def invalidate_token(self):
+    headers = {
+      'Authorization': f'Bearer {self.access_token}'
+    }
+    response = requests.delete(url=f'{HH_API_BASE}/token', headers=headers)
+    return response.json()
+  
+  def get_info(self):
+    headers = {
+      'HH-User-Agent': self.appdata.client_info,
+      'Authorization': f'Bearer {self.access_token}'
+    }
+    params = {
+      'locale': self.appdata.locale,
+      'host': self.appdata.host
+    }
+    response = requests.get(url=f'{HH_API_BASE}/me', headers=headers, params=params)
+    return response.json()
+
+  def get_resumes(self):
+    headers = {
+      'HH-User-Agent': self.appdata.client_info,
+      'Authorization': f'Bearer {self.access_token}'
+    }
+    params = {
+      'locale': self.appdata.locale,
+      'host': self.appdata.host
+    }
+    response = requests.get(url=f'{HH_API_BASE}/resumes/mine', headers=headers, params=params)
+    return response.json()
+  
+  def get_vacancies_for_resume(self, resume_id: str, params: dict):
+    headers = {
+      'HH-User-Agent': self.appdata.client_info,
+      'Authorization': f'Bearer {self.access_token}'
+    }
+    response = requests.get(url=f'{HH_API_BASE}/resumes/{resume_id}/similar_vacancies', headers=headers, params=params)
+    return response.json()
+
+    
+
+
+if __name__ == '__main__':
+  pass
---
+++ main.py
@@ -0,0 +1,54 @@
+from hh_api import core
+from dotenv import load_dotenv
+import os
+import pymongo
+import time
+
+load_dotenv()
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+app_token = os.getenv('APP_TOKEN')
+mongo_connection= os.getenv('MONGO_CONNECTION')
+
+myclient = pymongo.MongoClient(mongo_connection)
+mydb = myclient["users"]
+users = mydb["users"]
+
+app = core.App(client_id, client_secret, 'boyceing/1 (boyceing@boyceing.ru)', 'hh.ru', app_token=app_token)
+
+target_user = 'akmd.uk@gmail.com'
+userinfo = users.find_one({'email': target_user}, {'_id': False})
+
+#  User not found in database
+if userinfo is None:
+  print(app.get_link_for_authcode())
+  emp = core.Employee(app, code=input())
+
+  user_profile = emp.get_info()
+  user_profile['access_token'] = emp.access_token
+  user_profile['refresh_token'] = emp.refresh_token
+  user_profile['expires_at'] = emp.expires_at
+
+  users.insert_one(user_profile)
+
+#  Found. If token expired - renew 
+else:
+  emp = core.Employee(app, employee_data=userinfo)
+  if userinfo['expires_at'] < time.time():
+    emp.renew_token()
+    query_filter = {'email' : target_user}
+    update_operation = { '$set' : {
+        'access_token': emp.access_token,
+        'refresh_token': emp.refresh_token,
+        'expires_at': emp.expires_at
+      }
+    }
+    result = users.update_one(query_filter, update_operation)
+
+# https://api.hh.ru/openapi/redoc#tag/Poisk-vakansij-dlya-soiskatelya/operation/get-vacancies-similar-to-resume
+search_filter = {
+  'salary': 200000
+}
+
+resume_id = emp.get_resumes()['items'][0]['id']
+print(emp.get_vacancies_for_resume(resume_id, search_filter))
